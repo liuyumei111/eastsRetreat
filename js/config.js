171e4b48ef5b4e9733daa29fd1ae26737099b11d
liuyumei111 <1712012403@qq.com>
2017-09-18 15:17:28 +0800

    0

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@
     C.market=marketHost+'/market/mobile/';
 
     // C.marketToken=localStorage.getItem('token');
-    //localStorage.setItem('token','2017091309212205066bc4aa3fda034b21abce358c08edb01f');
+    localStorage.setItem('token','201709181509405117a680d79b7f3f4bfc9c0b497395b511bb');
     C.marketToken=localStorage.getItem('token');
     //获取微信oppenId
     C.getWxUserInfo='http://www.rrfun.com.cn/Uc/getInfo';
